09a93ba3c677ff60c10754102e5df6dadf58d7be
n-malikov <nail1000@gmail.com>
2026-02-06 22:01:09 +0300

    sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,6 @@ export default defineNuxtConfig({
     },
   },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/scss/main.scss']
 })

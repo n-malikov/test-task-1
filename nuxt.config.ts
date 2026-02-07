35301fe3a370f2dc312dde1189e95084b1c16b03
n-malikov <nail1000@gmail.com>
2026-02-07 14:33:07 +0300

    add valibot and nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,8 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+  ],
   css: ['~/assets/scss/main.scss']
 })
